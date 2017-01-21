e90a5b30fbe6d8d1982dc93df9ca6d6d9098c8f9
pwnedpixel <s1l3nt.n00bz@gmail.com>
2017-01-21 18:55:59 -0500

    broke some stuff. its not posting

--- src/modules/readCaptionAloud.py
+++ src/modules/readCaptionAloud.py
@@ -1,12 +1,27 @@
-import talkey
-from gtts import gTTS
-from pygame import mixer
 import subprocess
+import requests as r
+import json
 
 def readText(textToRead):
+    open_file = open('../keys', 'r')
+    file_lines = open_file.readlines()
+    nmaid = file_lines[3].strip()
+    nmaidKey = file_lines[4].strip()
+
+
     print("reading...")
-    tts = gTTS(text=textToRead, lang='en')
-    tts.save("speech.mp3")
+    url = "https://nim-rd.nuance.mobi:9443/nina-webapi/TTS/"
+    headers = {'nmaid': nmaid, 'nmaidkey':nmaidKey, 'Content-Type':'application/json'}
+    body = {
+        'text': textToRead,
+        'tts-type': 'text',
+        'user': 'http_sample'
+        }
+    print("send post")
+    response = r.post(url, headers=headers, data=json.dumps(body))
+    print("got response")
+    print(response)
+
     print("...done")
 
     #subprocess.call(["/bin/mv","/tmp/a","/tmp/b"])
