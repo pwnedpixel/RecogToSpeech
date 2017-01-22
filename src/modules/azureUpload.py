b1644e3df869856de5cd98cce8975f7482a27e45
pwnedpixel <s1l3nt.n00bz@gmail.com>
2017-01-21 20:07:20 -0500

    Added image name consistency and incrementation

--- src/modules/azureUpload.py
+++ src/modules/azureUpload.py
@@ -4,17 +4,45 @@ from time import gmtime, strftime
 
 def uploadImg():
     print("uploading...")
-    #get key from file
+
+    #get api key from file
     open_file = open('../keys', 'r')
     file_lines = open_file.readlines()
     key = file_lines[2].strip()
+    file_service = FileService(account_name='recog', account_key=key)
 
-    #upload to server
-    file_service = FileService (account_name='recog', account_key=key)
+    #fetch the index file from server
+    file_service.get_file_to_path('recogimages', None, 'current.txt', '../current.txt')
+
+    #open the index file and get the index value
+    open_file = open('../current.txt', 'r')
+    file_lines = open_file.readlines()
+    currentIndex = int(file_lines[0].strip())
+    open_file.close()
+
+    #increase the index
+    currentIndex+=1
+
+    #upload picture to server
     file_service.create_file_from_path(
         'recogimages',
         None, # We want to create this blob in the root directory, so we specify None for the directory_name
-        strftime("%Y-%m-%d-%H-%M-%S", gmtime())+'.jpg',
+        strftime("image"+str(currentIndex), gmtime())+'.jpg',
         'img.jpg',
         content_settings=ContentSettings(content_type='image/jpg'))
+
+    #open index file in write mode, and write new index
+    open_file = open('../current.txt', 'w')
+    open_file.writelines(str(currentIndex))
+    open_file.close()
+
+    #upload index file back to server
+    file_service.create_file_from_path(
+        'recogimages',
+        None,  # We want to create this blob in the root directory, so we specify None for the directory_name
+        'current.txt',
+        '../current.txt',
+        content_settings=ContentSettings(content_type='text/plain'))
+
+
     print("...done")
